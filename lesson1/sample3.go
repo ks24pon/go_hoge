088234d960c7802db510235d5a579d8fc696db53
test <testtest@tesutonoMacBook-Air.local>
2024-09-16 16:00:01 +0900

    map

---
+++ lesson1/sample3.go
@@ -0,0 +1,22 @@
+package main
+
+func main() {
+	m := map[string]int{"x": 10, "y": 20}
+
+	// キーを指定アクセス
+	println(m["x"])
+
+	// キーを指定して入力
+	m["z"] = 30
+
+	// 存在を確認
+	n, ok := m["z"]
+	println(n, ok)
+
+	// キーを指定して削除
+	delete(m, "z")
+
+	// 削除されていることを確認
+	n, ok = m["z"]
+	println(n, ok)
+}
